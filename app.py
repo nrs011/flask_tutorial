49916407a669f1cd671bb147195bd4824872dd07
Nimesh Ryan Silva <nimeshsilva@gmail.com>
2021-03-13 11:53:14 -0500

    added html files

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from flask import render_template
+
 from flask import Flask
 app = Flask(__name__)
 
@@ -5,3 +7,37 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Hello, Flask!"
+
+
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello_there(name=None):
+    return render_template(
+        "hello_there.html",
+        name=name,
+        date=datetime.now()
+    )
+
+
+@app.route("/api/data")
+def get_data():
+    return app.send_static_file("data.json")
+
+# Replace the existing home function with the one below
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+# New functions
+
+
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
